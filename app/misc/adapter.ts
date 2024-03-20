65f6d1c625605f2093e6df8915edfdf20122a2f6
NB😈 <norbertbodziony@gmail.com>
2024-03-20 14:22:59 +0100

    fix

--- app/misc/adapter.ts
+++ app/misc/adapter.ts
@@ -1,18 +1,15 @@
 import { NightlyConnectAdapter } from '@nightlylabs/wallet-selector-polkadot'
 
 let _adapter: NightlyConnectAdapter | undefined
-export const getAdapter = async (persisted = true) => {
+export const getAdapter = async () => {
   if (_adapter) return _adapter
-  _adapter = await NightlyConnectAdapter.build(
-    {
-      appMetadata: {
-        name: 'Vara Template',
-        description: 'Vara Template',
-        icon: 'https://docs.nightly.app/img/logo.png',
-      },
-      network: 'Vara',
+  _adapter = await NightlyConnectAdapter.build({
+    appMetadata: {
+      name: 'Vara Template',
+      description: 'Vara Template',
+      icon: 'https://docs.nightly.app/img/logo.png',
     },
-    persisted
-  )
+    network: 'Vara',
+  })
   return _adapter
 }
